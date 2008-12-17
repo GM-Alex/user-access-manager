38107c8d718946d3b4df1c0912363039e09bbc45
GM_Alex <GM_Alex@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-12-17 20:36:10 +0000

    git-svn-id: http://plugins.svn.wordpress.org/user-access-manager/tags/0.61@80794 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- user-access-manager.php
+++ user-access-manager.php
@@ -3,7 +3,7 @@
 Plugin Name: User Access Manager
 Plugin URI: http://www.gm-alex.de/projects/wordpress/plugins/user-access-manager/
 Author URI: http://www.gm-alex.de/
-Version: 0.6
+Version: 0.61
 Author: Alexander Schneider
 Description: Manage the access to your posts and pages. <strong>Note:</strong> <em>If you activate the plugin your upload dir will protect by a '.htaccess' with a random password and all old downloads insert in a previous post/page will not work anymore. You have to update your posts/pages. If you use already a '.htaccess' file to protect your files the plugin will not overwrite the '.htaccess'.</em>
  
