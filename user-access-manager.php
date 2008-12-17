4d50fb20ad6011f545ac696fe0c2e58dfab4cdc5
GM_Alex <GM_Alex@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-12-17 20:43:17 +0000

    git-svn-id: http://plugins.svn.wordpress.org/user-access-manager/tags/0.61@80800 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- user-access-manager.php
+++ user-access-manager.php
@@ -1128,11 +1128,6 @@ if (!class_exists("UserAccessManager"))
 			
 			return $page;
 		}
-		
-		function show_categoriey($category)
-		{
-			
-		}
 	
 		function redirect_user()
 		{
@@ -1324,9 +1319,6 @@ if (isset($userAccessManager))
 		
 	if($uamOptions['hide_post'] == 'true')
 		add_filter('the_posts', array(&$userAccessManager, 'show_post'), 1);
-		
-	if($uamOptions['hide_categories'] == 'true')
-		add_filter('the_posts', array(&$userAccessManager, 'show_category'), 1);
 }
 
 ?>
