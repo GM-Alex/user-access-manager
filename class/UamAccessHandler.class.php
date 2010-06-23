899316a078bec8f52bbd85fb9433dc5088f9902d
GM_Alex <GM_Alex@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-23 17:54:18 +0000

    git-svn-id: http://plugins.svn.wordpress.org/user-access-manager/trunk@256600 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ class/UamAccessHandler.class.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * UamAccessHandler.class.php
+ * 
+ * The UamUserGroup class file.
+ * 
+ * PHP versions 5
+ * 
+ * @category  UserAccessManager
+ * @package   UserAccessManager
+ * @author    Alexander Schneider <alexanderschneider85@googlemail.com>
+ * @copyright 2008-2010 Alexander Schneider
+ * @license   http://www.gnu.org/licenses/gpl-2.0.html  GNU General Public License, version 2
+ * @version   SVN: $Id$
+ * @link      http://wordpress.org/extend/plugins/user-access-manager/
+ */
+
+/**
+ * The access handler class.
+ * 
+ * @category UserAccessManager
+ * @package  UserAccessManager
+ * @author   Alexander Schneider <alexanderschneider85@gmail.com>
+ * @license  http://www.gnu.org/licenses/gpl-2.0.html  GNU General Public License, version 2
+ * @link     http://wordpress.org/extend/plugins/user-access-manager/
+ */
+
+class UamAccessHandler
+{
+
+}
