f3b4582e61a212f6c5536384718b01ca56571077
Alexander Schneider <alexanderschneider85@gmail.com>
2017-03-18 23:47:33 +0100

    Adjust phpunit config Some more controller tests Improve namespace defines

--- src/UserAccessManager/Wrapper/Wordpress.php
+++ src/UserAccessManager/Wrapper/Wordpress.php
@@ -44,7 +44,7 @@ class Wordpress
     }
 
     /**
-     * @see is_post_type_hierarchical()
+     * @see \is_post_type_hierarchical()
      *
      * @param string $sPostType
      *
@@ -52,11 +52,11 @@ class Wordpress
      */
     public function isPostTypeHierarchical($sPostType)
     {
-        return is_post_type_hierarchical($sPostType);
+        return \is_post_type_hierarchical($sPostType);
     }
 
     /**
-     * @see is_taxonomy_hierarchical()
+     * @see \is_taxonomy_hierarchical()
      *
      * @param string $sTaxonomy
      *
@@ -64,11 +64,11 @@ class Wordpress
      */
     public function isTaxonomyHierarchical($sTaxonomy)
     {
-        return is_taxonomy_hierarchical($sTaxonomy);
+        return \is_taxonomy_hierarchical($sTaxonomy);
     }
 
     /**
-     * @see get_userdata()
+     * @see \get_userdata()
      *
      * @param string $sId
      *
@@ -76,11 +76,11 @@ class Wordpress
      */
     public function getUserData($sId)
     {
-        return get_userdata($sId);
+        return \get_userdata($sId);
     }
 
     /**
-     * @see get_post_types()
+     * @see \get_post_types()
      *
      * @param array  $aArguments
      * @param string $sOutput
@@ -90,11 +90,11 @@ class Wordpress
      */
     public function getPostTypes(array $aArguments = array(), $sOutput = 'names', $sOperator = 'and')
     {
-        return get_post_types($aArguments, $sOutput, $sOperator);
+        return \get_post_types($aArguments, $sOutput, $sOperator);
     }
 
     /**
-     * @see get_taxonomies()
+     * @see \get_taxonomies()
      *
      * @param array  $aArguments
      * @param string $sOutput
@@ -104,11 +104,11 @@ class Wordpress
      */
     public function getTaxonomies(array $aArguments = array(), $sOutput = 'names', $sOperator = 'and')
     {
-        return get_taxonomies($aArguments, $sOutput, $sOperator);
+        return \get_taxonomies($aArguments, $sOutput, $sOperator);
     }
 
     /**
-     * @see get_post()
+     * @see \get_post()
      *
      * @param string $sId The post id.
      * @param string $sOutput
@@ -118,11 +118,11 @@ class Wordpress
      */
     public function getPost($sId, $sOutput = OBJECT, $sFilter = 'raw')
     {
-        return get_post($sId, $sOutput, $sFilter);
+        return \get_post($sId, $sOutput, $sFilter);
     }
 
     /**
-     * @see get_term()
+     * @see \get_term()
      *
      * @param string $sId
      * @param string $sTaxonomy
@@ -133,12 +133,12 @@ class Wordpress
      */
     public function getTerm($sId, $sTaxonomy = '', $sOutput = OBJECT, $sFilter = 'raw')
     {
-        return get_term($sId, $sTaxonomy, $sOutput, $sFilter);
+        return \get_term($sId, $sTaxonomy, $sOutput, $sFilter);
     }
 
 
     /**
-     * @see dbDelta()
+     * @see \dbDelta()
      *
      * @param array|string $mQueries
      * @param bool         $blExecute
@@ -147,11 +147,11 @@ class Wordpress
      */
     public function dbDelta($mQueries = '', $blExecute = true)
     {
-        return dbDelta($mQueries, $blExecute);
+        return \dbDelta($mQueries, $blExecute);
     }
 
     /**
-     * @see switch_to_blog()
+     * @see \switch_to_blog()
      *
      * @param integer $iBlogId
      *
@@ -159,32 +159,32 @@ class Wordpress
      */
     public function switchToBlog($iBlogId)
     {
-        return switch_to_blog($iBlogId);
+        return \switch_to_blog($iBlogId);
     }
 
     /**
-     * @see is_multisite()
+     * @see \is_multisite()
      *
      * @return bool
      */
     public function isMultiSite()
     {
-        return is_multisite();
+        return \is_multisite();
     }
 
     /**
-     * @see do_action()
+     * @see \do_action()
      *
      * @param string $sTag
      * @param mixed  $mArguments
      */
     public function doAction($sTag, $mArguments = '')
     {
-        do_action($sTag, $mArguments);
+        \do_action($sTag, $mArguments);
     }
 
     /**
-     * @see add_action()
+     * @see \add_action()
      *
      * @param string   $sTag
      * @param callable $mFunctionToAdd
@@ -195,11 +195,11 @@ class Wordpress
      */
     public function addAction($sTag, $mFunctionToAdd, $iPriority = 10, $iAcceptedArguments = 1)
     {
-        return add_action($sTag, $mFunctionToAdd, $iPriority, $iAcceptedArguments);
+        return \add_action($sTag, $mFunctionToAdd, $iPriority, $iAcceptedArguments);
     }
 
     /**
-     * @see has_filter()
+     * @see \has_filter()
      *
      * @param string        $sTag
      * @param bool|callable $mFunctionToCheck
@@ -208,11 +208,11 @@ class Wordpress
      */
     public function hasFilter($sTag, $mFunctionToCheck = false)
     {
-        return has_filter($sTag, $mFunctionToCheck);
+        return \has_filter($sTag, $mFunctionToCheck);
     }
 
     /**
-     * @see add_filter()
+     * @see \add_filter()
      *
      * @param string   $sTag
      * @param callable $mFunctionToAdd
@@ -223,11 +223,11 @@ class Wordpress
      */
     public function addFilter($sTag, $mFunctionToAdd, $iPriority = 10, $iAcceptedArguments = 1)
     {
-        return add_filter($sTag, $mFunctionToAdd, $iPriority, $iAcceptedArguments);
+        return \add_filter($sTag, $mFunctionToAdd, $iPriority, $iAcceptedArguments);
     }
 
     /**
-     * @see remove_filter()
+     * @see \remove_filter()
      *
      * @param string   $sTag
      * @param callable $mFunctionToRemove
@@ -237,11 +237,11 @@ class Wordpress
      */
     public function removeFilter($sTag, $mFunctionToRemove, $iPriority = 10)
     {
-        return remove_filter($sTag, $mFunctionToRemove, $iPriority);
+        return \remove_filter($sTag, $mFunctionToRemove, $iPriority);
     }
 
     /**
-     * @see add_option()
+     * @see \add_option()
      *
      * @param string      $sOption
      * @param mixed       $mValue
@@ -252,11 +252,11 @@ class Wordpress
      */
     public function addOption($sOption, $mValue = '', $sDeprecated = '', $mAutoload = 'yes')
     {
-        return add_option($sOption, $mValue, $sDeprecated, $mAutoload);
+        return \add_option($sOption, $mValue, $sDeprecated, $mAutoload);
     }
 
     /**
-     * @see delete_option()
+     * @see \delete_option()
      *
      * @param string $sOption
      *
@@ -264,10 +264,12 @@ class Wordpress
      */
     public function deleteOption($sOption)
     {
-        return delete_option($sOption);
+        return \delete_option($sOption);
     }
 
     /**
+     * @see \update_option()
+     * 
      * @param string      $sOption
      * @param mixed       $mValue
      * @param string|bool $mAutoload
@@ -276,11 +278,11 @@ class Wordpress
      */
     public function updateOption($sOption, $mValue = '', $mAutoload = 'yes')
     {
-        return update_option($sOption, $mValue, $mAutoload);
+        return \update_option($sOption, $mValue, $mAutoload);
     }
 
     /**
-     * @see get_option()
+     * @see \get_option()
      *
      * @param string $sOption
      * @param bool   $blDefault
@@ -289,11 +291,11 @@ class Wordpress
      */
     public function getOption($sOption, $blDefault = false)
     {
-        return get_option($sOption, $blDefault);
+        return \get_option($sOption, $blDefault);
     }
 
     /**
-     * @see is_super_admin()
+     * @see \is_super_admin()
      *
      * @param bool $sUserId
      *
@@ -301,21 +303,21 @@ class Wordpress
      */
     public function isSuperAdmin($sUserId = false)
     {
-        return is_super_admin($sUserId);
+        return \is_super_admin($sUserId);
     }
 
     /**
-     * @see wp_get_current_user()
+     * @see \wp_get_current_user()
      *
      * @return \WP_User
      */
     public function getCurrentUser()
     {
-        return wp_get_current_user();
+        return \wp_get_current_user();
     }
 
     /**
-     * @see get_allowed_mime_types()
+     * @see \get_allowed_mime_types()
      *
      * @param int|\WP_User $mUser
      *
@@ -323,11 +325,11 @@ class Wordpress
      */
     public function getAllowedMimeTypes($mUser = null)
     {
-        return get_allowed_mime_types($mUser);
+        return \get_allowed_mime_types($mUser);
     }
 
     /**
-     * @see wp_upload_dir()
+     * @see \wp_upload_dir()
      *
      * @param string $sTime
      * @param bool   $blCreateDir
@@ -337,11 +339,11 @@ class Wordpress
      */
     public function getUploadDir($sTime = null, $blCreateDir = true, $blRefreshCache = false)
     {
-        return wp_upload_dir($sTime, $blCreateDir, $blRefreshCache);
+        return \wp_upload_dir($sTime, $blCreateDir, $blRefreshCache);
     }
 
     /**
-     * @see home_url()
+     * @see \home_url()
      *
      * @param string $sPath
      * @param string $sScheme
@@ -350,11 +352,11 @@ class Wordpress
      */
     public function getHomeUrl($sPath = '', $sScheme = null)
     {
-        return home_url($sPath, $sScheme);
+        return \home_url($sPath, $sScheme);
     }
 
     /**
-     * @see wp_parse_id_list()
+     * @see \wp_parse_id_list()
      *
      * @param array|string $mList
      *
@@ -362,11 +364,11 @@ class Wordpress
      */
     public function parseIdList($mList)
     {
-        return wp_parse_id_list($mList);
+        return \wp_parse_id_list($mList);
     }
 
     /**
-     * @see wp_die()
+     * @see \wp_die()
      *
      * @param string $sMessage
      * @param string $sTitle
@@ -374,11 +376,11 @@ class Wordpress
      */
     public function wpDie($sMessage = '', $sTitle = '', array $aArguments = array())
     {
-        wp_die($sMessage, $sTitle, $aArguments);
+        \wp_die($sMessage, $sTitle, $aArguments);
     }
 
     /**
-     * @see is_feed()
+     * @see \is_feed()
      *
      * @param string|array $mFeeds
      *
@@ -386,21 +388,21 @@ class Wordpress
      */
     public function isFeed($mFeeds = '')
     {
-        return is_feed($mFeeds);
+        return \is_feed($mFeeds);
     }
 
     /**
-     * @see is_user_logged_in()
+     * @see \is_user_logged_in()
      *
      * @return bool
      */
     public function isUserLoggedIn()
     {
-        return is_user_logged_in();
+        return \is_user_logged_in();
     }
 
     /**
-     * @see get_page_by_path()
+     * @see \get_page_by_path()
      *
      * @param string $sPagePath
      * @param string $sOutput
@@ -410,11 +412,11 @@ class Wordpress
      */
     public function getPageByPath($sPagePath, $sOutput = OBJECT, $sPostType = 'page')
     {
-        return get_page_by_path($sPagePath, $sOutput, $sPostType);
+        return \get_page_by_path($sPagePath, $sOutput, $sPostType);
     }
 
     /**
-     * @see wp_redirect()
+     * @see \wp_redirect()
      *
      * @param string $sLocation
      * @param int    $iStatus
@@ -423,7 +425,7 @@ class Wordpress
      */
     public function wpRedirect($sLocation, $iStatus = 302)
     {
-        return wp_redirect($sLocation, $iStatus);
+        return \wp_redirect($sLocation, $iStatus);
     }
 
     /**
@@ -435,7 +437,7 @@ class Wordpress
      */
     public function getPageLink($mPost = false, $blLeaveName = false, $blSample = false)
     {
-        return get_page_link($mPost, $blLeaveName, $blSample);
+        return \get_page_link($mPost, $blLeaveName, $blSample);
     }
 
     /**
@@ -450,17 +452,17 @@ class Wordpress
     }
 
     /**
-     * @see is_admin()
+     * @see \is_admin()
      *
      * @return bool
      */
     public function isAdmin()
     {
-        return is_admin();
+        return \is_admin();
     }
 
     /**
-     * @see wp_create_nonce()
+     * @see \wp_create_nonce()
      *
      * @param int|string $mAction
      *
@@ -468,11 +470,11 @@ class Wordpress
      */
     public function createNonce($mAction)
     {
-        return wp_create_nonce($mAction);
+        return \wp_create_nonce($mAction);
     }
 
     /**
-     * @see wp_nonce_field()
+     * @see \wp_nonce_field()
      *
      * @param int|string $mAction
      * @param string     $sName
@@ -483,11 +485,11 @@ class Wordpress
      */
     public function getNonceField($mAction = -1, $sName = '_wpnonce', $blReferrer = true, $blEcho = true)
     {
-        return wp_nonce_field($mAction, $sName, $blReferrer, $blEcho);
+        return \wp_nonce_field($mAction, $sName, $blReferrer, $blEcho);
     }
 
     /**
-     * @see wp_verify_nonce()
+     * @see \wp_verify_nonce()
      *
      * @param string     $sNonce
      * @param string|int $mAction
@@ -496,7 +498,7 @@ class Wordpress
      */
     public function verifyNonce($sNonce, $mAction = -1)
     {
-        return wp_verify_nonce($sNonce, $mAction);
+        return \wp_verify_nonce($sNonce, $mAction);
     }
 
     /**
@@ -511,7 +513,7 @@ class Wordpress
     }
 
     /**
-     * @see add_menu_page()
+     * @see \add_menu_page()
      *
      * @param string $sPageTitle
      * @param string $sMenuTitle
@@ -525,11 +527,11 @@ class Wordpress
      */
     public function addMenuPage($sPageTitle, $sMenuTitle, $sCapability, $sMenuSlug, $cFunction = '', $sIconUrl = '', $iPosition = null)
     {
-        return add_menu_page($sPageTitle, $sMenuTitle, $sCapability, $sMenuSlug, $cFunction, $sIconUrl, $iPosition);
+        return \add_menu_page($sPageTitle, $sMenuTitle, $sCapability, $sMenuSlug, $cFunction, $sIconUrl, $iPosition);
     }
 
     /**
-     * @see add_submenu_page()
+     * @see \add_submenu_page()
      *
      * @param string $sParentSlug
      * @param string $sPageTitle
@@ -542,11 +544,11 @@ class Wordpress
      */
     public function addSubMenuPage($sParentSlug, $sPageTitle, $sMenuTitle, $sCapability, $sMenuSlug, $cFunction = '')
     {
-        return add_submenu_page($sParentSlug, $sPageTitle, $sMenuTitle, $sCapability, $sMenuSlug, $cFunction);
+        return \add_submenu_page($sParentSlug, $sPageTitle, $sMenuTitle, $sCapability, $sMenuSlug, $cFunction);
     }
 
     /**
-     * @see add_meta_box()
+     * @see \add_meta_box()
      *
      * @param string                  $sId
      * @param string                  $sTitle
@@ -566,11 +568,11 @@ class Wordpress
         $aCallbackArguments = null
     )
     {
-        return add_meta_box($sId, $sTitle, $cCallback, $mScreen, $sContext, $sPriority, $aCallbackArguments);
+        \add_meta_box($sId, $sTitle, $cCallback, $mScreen, $sContext, $sPriority, $aCallbackArguments);
     }
 
     /**
-     * @see get_pages()
+     * @see \get_pages()
      *
      * @param array|string $mArguments
      *
@@ -578,11 +580,11 @@ class Wordpress
      */
     public function getPages($mArguments)
     {
-        return get_pages($mArguments);
+        return \get_pages($mArguments);
     }
 
     /**
-     * @see wp_register_style()
+     * @see \wp_register_style()
      *
      * @param string           $sHandle
      * @param string           $sSource
@@ -594,11 +596,11 @@ class Wordpress
      */
     public function registerStyle($sHandle, $sSource, $aDepends = array(), $mVersion = false, $sMedia = 'all')
     {
-        return wp_register_style($sHandle, $sSource, $aDepends, $mVersion, $sMedia);
+        return \wp_register_style($sHandle, $sSource, $aDepends, $mVersion, $sMedia);
     }
 
     /**
-     * @see wp_register_script()
+     * @see \wp_register_script()
      *
      * @param string           $sHandle
      * @param string           $sSource
@@ -610,11 +612,11 @@ class Wordpress
      */
     public function registerScript($sHandle, $sSource, $aDepends = array(), $mVersion = false, $blInFooter = false)
     {
-        return wp_register_script($sHandle, $sSource, $aDepends, $mVersion, $blInFooter);
+        return \wp_register_script($sHandle, $sSource, $aDepends, $mVersion, $blInFooter);
     }
 
     /**
-     * @see wp_enqueue_style()
+     * @see \wp_enqueue_style()
      *
      * @param string           $sHandle
      * @param string           $sSource
@@ -624,11 +626,11 @@ class Wordpress
      */
     public function enqueueStyle($sHandle, $sSource = '', $aDepends = array(), $mVersion = false, $sMedia = 'all')
     {
-        wp_enqueue_style($sHandle, $sSource, $aDepends, $mVersion, $sMedia);
+        \wp_enqueue_style($sHandle, $sSource, $aDepends, $mVersion, $sMedia);
     }
 
     /**
-     * @see wp_enqueue_script()
+     * @see \wp_enqueue_script()
      *
      * @param string           $sHandle
      * @param string           $sSource
@@ -638,7 +640,7 @@ class Wordpress
      */
     public function enqueueScript($sHandle, $sSource = '', $aDepends = array(), $mVersion = false, $blInFooter = false)
     {
-        wp_enqueue_script($sHandle, $sSource, $aDepends, $mVersion, $blInFooter);
+        \wp_enqueue_script($sHandle, $sSource, $aDepends, $mVersion, $blInFooter);
     }
 
     /**
@@ -653,7 +655,7 @@ class Wordpress
     }
 
     /**
-     * @see get_sites()
+     * @see \get_sites()
      *
      * @param array $aArguments
      *
@@ -661,11 +663,11 @@ class Wordpress
      */
     public function getSites(array $aArguments = array())
     {
-        return get_sites($aArguments);
+        return \get_sites($aArguments);
     }
 
     /**
-     * @see apply_filters()
+     * @see \apply_filters()
      *
      * @param string $sTag
      * @param mixed  $mValue
@@ -674,11 +676,11 @@ class Wordpress
      */
     public function applyFilters($sTag, $mValue)
     {
-        return apply_filters($sTag, $mValue);
+        return \apply_filters($sTag, $mValue);
     }
 
     /**
-     * @see get_bloginfo()
+     * @see \get_bloginfo()
      *
      * @param string $sShow
      * @param string $sFilter
@@ -687,11 +689,11 @@ class Wordpress
      */
     public function getBlogInfo($sShow = '', $sFilter = 'raw')
     {
-        return get_bloginfo($sShow, $sFilter);
+        return \get_bloginfo($sShow, $sFilter);
     }
 
     /**
-     * @see esc_html()
+     * @see \esc_html()
      *
      * @param string $sText
      *
@@ -699,11 +701,11 @@ class Wordpress
      */
     public function escHtml($sText)
     {
-        return esc_html($sText);
+        return \esc_html($sText);
     }
 
     /**
-     * @see is_single()
+     * @see \is_single()
      *
      * @param int|string|array $mPost
      *
@@ -711,11 +713,11 @@ class Wordpress
      */
     public function isSingle($mPost = '')
     {
-        return is_single($mPost);
+        return \is_single($mPost);
     }
 
     /**
-     * @see is_page()
+     * @see \is_page()
      *
      * @param int|string|array $mPage
      *
@@ -723,11 +725,11 @@ class Wordpress
      */
     public function isPage($mPage = '')
     {
-        return is_page($mPage);
+        return \is_page($mPage);
     }
 
     /**
-     * @see WP_PLUGIN_DIR
+     * @see \WP_PLUGIN_DIR
      *
      * @return string
      */
@@ -737,7 +739,7 @@ class Wordpress
     }
 
     /**
-     * @see plugins_url()
+     * @see \plugins_url()
      *
      * @param string $sPath
      * @param string $sPlugin
@@ -746,11 +748,11 @@ class Wordpress
      */
     public function pluginsUrl($sPath = '', $sPlugin = '')
     {
-        return plugins_url($sPath, $sPlugin);
+        return \plugins_url($sPath, $sPlugin);
     }
 
     /**
-     * @see plugin_basename()
+     * @see \plugin_basename()
      *
      * @param $sFile
      *
@@ -758,6 +760,6 @@ class Wordpress
      */
     public function pluginBasename($sFile)
     {
-        return plugin_basename($sFile);
+        return \plugin_basename($sFile);
     }
 }
--- tests/unit/UserAccessManager/Controller/AdminObjectControllerTest.php
+++ tests/unit/UserAccessManager/Controller/AdminObjectControllerTest.php
@@ -37,4 +37,158 @@ class AdminObjectControllerTest extends \UserAccessManagerTestCase
 
         self::assertInstanceOf('\UserAccessManager\Controller\AdminObjectController', $oAdminObjectController);
     }
+
+    /**
+     * @group  unit
+     * @covers \UserAccessManager\Controller\AdminObjectController::_setObjectInformation()
+     *
+     * @return AdminObjectController
+     */
+    public function testSetObjectInformation()
+    {
+        $aFullGroups = [
+            1 => $this->getUserGroup(1),
+            2 => $this->getUserGroup(2)
+        ];
+
+        $aFilteredGroups = [
+            1 => $this->getUserGroup(1)
+        ];
+
+        $oAccessHandler = $this->getAccessHandler();
+
+        $oAccessHandler->expects($this->once())
+            ->method('getUserGroupsForObject')
+            ->with('objectType', 'objectId')
+            ->will($this->returnValue($aFullGroups));
+
+        $oAccessHandler->expects($this->once())
+            ->method('getFilteredUserGroupsForObject')
+            ->with('objectType', 'objectId')
+            ->will($this->returnValue($aFilteredGroups));
+
+
+        $oAdminObjectController = new AdminObjectController(
+            $this->getWrapper(),
+            $this->getConfig(),
+            $this->getDatabase(),
+            $this->getObjectHandler(),
+            $oAccessHandler
+        );
+
+        self::callMethod($oAdminObjectController, '_setObjectInformation', ['objectType', 'objectId']);
+
+        self::assertAttributeEquals('objectType', '_sObjectType', $oAdminObjectController);
+        self::assertAttributeEquals('objectId', '_sObjectId', $oAdminObjectController);
+        self::assertAttributeEquals($aFullGroups, '_aFullObjectUserGroups', $oAdminObjectController);
+        self::assertAttributeEquals($aFilteredGroups, '_aObjectUserGroups', $oAdminObjectController);
+        self::assertAttributeEquals(1, '_iUserGroupDiff', $oAdminObjectController);
+
+        return $oAdminObjectController;
+    }
+
+    /**
+     * @group   unit
+     * @covers  \UserAccessManager\Controller\AdminObjectController::getObjectType()
+     * @depends testSetObjectInformation
+     *
+     * @param AdminObjectController $oAdminObjectController
+     */
+    public function testGetObjectType(AdminObjectController $oAdminObjectController)
+    {
+
+    }
+
+    /**
+     * @group   unit
+     * @covers  \UserAccessManager\Controller\AdminObjectController::getObjectId()
+     * @depends testSetObjectInformation
+     *
+     * @param AdminObjectController $oAdminObjectController
+     */
+    public function testGetObjectId(AdminObjectController $oAdminObjectController)
+    {
+
+    }
+
+    /**
+     * @group   unit
+     * @covers  \UserAccessManager\Controller\AdminObjectController::getFullObjectUserGroups()
+     * @depends testSetObjectInformation
+     *
+     * @param AdminObjectController $oAdminObjectController
+     */
+    public function testGetFullObjectUserGroups(AdminObjectController $oAdminObjectController)
+    {
+
+    }
+
+    /**
+     * @group   unit
+     * @covers  \UserAccessManager\Controller\AdminObjectController::getObjectUserGroups()
+     * @depends testSetObjectInformation
+     *
+     * @param AdminObjectController $oAdminObjectController
+     */
+    public function testGetObjectUserGroups(AdminObjectController $oAdminObjectController)
+    {
+
+    }
+
+    /**
+     * @group   unit
+     * @covers  \UserAccessManager\Controller\AdminObjectController::getUserGroupDiff()
+     * @depends testSetObjectInformation
+     *
+     * @param AdminObjectController $oAdminObjectController
+     */
+    public function testGetUserGroupDiff(AdminObjectController $oAdminObjectController)
+    {
+
+    }
+
+    /**
+     * @group  unit
+     * @covers \UserAccessManager\Controller\AdminObjectController::getUserGroups()
+     */
+    public function testGetUserGroups()
+    {
+
+    }
+
+    /**
+     * @group  unit
+     * @covers \UserAccessManager\Controller\AdminObjectController::isCurrentUserAdmin()
+     */
+    public function testIsCurrentUserAdmin()
+    {
+
+    }
+
+    /**
+     * @group  unit
+     * @covers \UserAccessManager\Controller\AdminObjectController::getRoleNames()
+     */
+    public function testGetRoleNames()
+    {
+
+    }
+
+    /**
+     * @group  unit
+     * @covers \UserAccessManager\Controller\AdminObjectController::getAllObjectTypes()
+     */
+    public function testGetAllObjectTypes()
+    {
+
+    }
+
+    /**
+     * @group  unit
+     * @covers \UserAccessManager\Controller\AdminObjectController::checkUserAccess()
+     */
+    public function testCheckUserAccess()
+    {
+
+    }
 }
