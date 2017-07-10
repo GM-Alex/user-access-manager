a2c29e79551c407632be721b776411c59a0370f0
Alexander Schneider <alexanderschneider85@gmail.com>
2017-07-10 12:32:16 +0200

    Adjust travis config

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,24 +1,6 @@
 #!/usr/bin/env bash
 set -e
 PLUGIN_ROOT="$(cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd)"
-composer install
-echo "Execute php codesniffer"
-${PLUGIN_ROOT}/vendor/bin/phpcs -p --standard=PSR2 ${PLUGIN_ROOT}/src ${PLUGIN_ROOT}/tests
-
-echo "Execute phpunit"
-${PLUGIN_ROOT}/vendor/bin/phpunit --coverage-clover=coverage.clover
-wget https://scrutinizer-ci.com/ocular.phar
-php ocular.phar code-coverage:upload --format=php-clover coverage.clover
-
-echo "Execute humbug"
-${PLUGIN_ROOT}/vendor/bin/humbug
-HUMBUG_REPORT_FILE=$(cat ${PLUGIN_ROOT}/humbuglog.json)
-REGEX='.*"total":[ ]*([0-9]+).*"kills":[ ]*([0-9]+).*'
-
-if [[ ! ${HUMBUG_REPORT_FILE} =~ ${REGEX} ]] || [[ ${BASH_REMATCH[1]} != ${BASH_REMATCH[2]} ]]; then
-    echo "Humbug mutation tests failed"
-    exit 1
-fi
 
 PLUGIN="user-access-manager"
 PLUGIN_BUILDS_PATH="${PLUGIN_ROOT}/builds/${PLUGIN}"
---
+++ scripts/humbug.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+PLUGIN_ROOT="$(cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd)"
+
+echo "Execute humbug"
+${PLUGIN_ROOT}/vendor/bin/humbug
+HUMBUG_REPORT_FILE=$(cat ${PLUGIN_ROOT}/humbuglog.json)
+REGEX='.*"total":[ ]*([0-9]+).*"kills":[ ]*([0-9]+).*'
+
+if [[ ! ${HUMBUG_REPORT_FILE} =~ ${REGEX} ]] || [[ ${BASH_REMATCH[1]} != ${BASH_REMATCH[2]} ]]; then
+    echo "Humbug mutation tests failed"
+    exit 1
+fi
