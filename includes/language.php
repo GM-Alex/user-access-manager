07a1cab105520d38f4d16755b615dacb36aa47e9
Alexander Schneider <alexanderschneider85@gmail.com>
2017-12-11 22:41:55 +0100

    Language adjustments

--- includes/language.php
+++ includes/language.php
@@ -159,6 +159,7 @@ define('TXT_UAM_PROTECT_FEED_DESC', __('Selecting "Yes" will also protect your f
 define('TXT_UAM_REDIRECT', __('Redirect user', 'user-access-manager'));
 define('TXT_UAM_REDIRECT_DESC', __('Setup what happen if a user visit a post/page with no access.', 'user-access-manager'));
 define('TXT_UAM_REDIRECT_TO_BLOG', __('To blog start page', 'user-access-manager'));
+define('TXT_UAM_REDIRECT_TO_LOGIN', __('To login page (wp-admin)', 'user-access-manager'));
 define('TXT_UAM_REDIRECT_TO_PAGE', __('Custom page: ', 'user-access-manager'));
 define('TXT_UAM_REDIRECT_TO_URL', __('Custom URL: ', 'user-access-manager'));
 define('TXT_UAM_LOCK_RECURSIVE', __('Lock recursive', 'user-access-manager'));
